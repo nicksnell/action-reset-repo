723f394d448c45db5755cb7d9afaee2baec3db3b
Nick Snell <nicksnell@pm.me>
2020-07-31 18:44:11 +0100

    fix: change to setup remote

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,7 +25,7 @@ git init
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 
-git remote set-url origin "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+git remote add origin "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 
 git fetch
 
