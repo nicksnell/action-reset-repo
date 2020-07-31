b0b5c31e982e4276288b7cb1ba22738480ce31bf
Nick Snell <nicksnell@pm.me>
2020-07-31 16:27:56 +0100

    Base

---
+++ README.md
@@ -0,0 +1,3 @@
+# Reset Git Branch
+
+Reset a specific branch to another. Useful if you need a specific branch to be reset to say master at given intervals. NOTE: this is a _reset_ and is **destructive** - you will lose any changes made to the branch being reset. Be sure this is what you want to do before running.
---
+++ entrypoint.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -l
+
+BASE_BRANCH=$1
+RESET_BRANCH=$2
+
+if [[ -z "${BASE_BRANCH}" ]]; then
+  echo "Missing \$BASE_BRANCH"
+  exit 1
+fi
+
+if [[ -z "${RESET_BRANCH}" ]]; then
+  echo "Missing \$RESET_BRANCH"
+  exit 1
+fi
+
+echo "BASE_BRANCH=${BASE_BRANCH}"
+echo "RESET_BRANCH=${RESET_BRANCH}"
+
+
+mkdir _tmp && cd _tmp
+git init
+
+git config user.name "${GITHUB_ACTOR}"
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+
+git remote add origin "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+
+git fetch
+
+git checkout "${RESET_BRANCH}"
+
+git reset --hard "${BASE_BRANCH}"
+
+git push --force origin "${RESET_BRANCH}"
